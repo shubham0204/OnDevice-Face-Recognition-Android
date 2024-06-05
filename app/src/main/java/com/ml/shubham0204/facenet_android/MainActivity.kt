1a6d44a0fd016bddf31ffc234ae8e459bf6fb924
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-06-05 08:53:35 +0530

    add navigation for screens in MainActivity.kt and Scaffold in all screens

--- app/src/main/java/com/ml/shubham0204/facenet_android/MainActivity.kt
+++ app/src/main/java/com/ml/shubham0204/facenet_android/MainActivity.kt
@@ -4,44 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ml.shubham0204.facenet_android.presentation.theme.FaceNetAndroidTheme
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ml.shubham0204.facenet_android.presentation.screens.AddFaceScreen
+import com.ml.shubham0204.facenet_android.presentation.screens.DetectScreen
+import com.ml.shubham0204.facenet_android.presentation.screens.FaceListScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FaceNetAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navHostController = rememberNavController()
+            NavHost(
+                navController = navHostController,
+                startDestination = "chat",
+                enterTransition = { fadeIn() },
+                exitTransition = { fadeOut() }
+            ) {
+                composable("add-face") {
+                    AddFaceScreen()
+                }
+                composable("detect") {
+                    DetectScreen()
+                }
+                composable("face-list"){
+                    FaceListScreen()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FaceNetAndroidTheme {
-        Greeting("Android")
-    }
 }
--- app/src/main/java/com/ml/shubham0204/facenet_android/presentation/screens/DetectScreen.kt
+++ app/src/main/java/com/ml/shubham0204/facenet_android/presentation/screens/DetectScreen.kt
@@ -1,7 +1,24 @@
 package com.ml.shubham0204.facenet_android.presentation.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ml.shubham0204.facenet_android.presentation.theme.FaceNetAndroidTheme
 
 @Composable
 fun DetectScreen() {
+    FaceNetAndroidTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) { ScreenUI() }
+        }
+    }
+}
+
+@Composable
+private fun ScreenUI() {
+    Text(text = "AddFaceScreen")
 }
